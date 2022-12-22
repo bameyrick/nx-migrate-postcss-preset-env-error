440268f5726836824ef49dd178474abaa03be6bc
Ben Meyrick (ext) <meyrick.ben.ext@siemens.com>
2022-12-22 09:46:46 +0000

    initial commit

---
+++ apps/migrate-postcss-preset-env-error/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'migrate-postcss-preset-env-error-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'migrate-postcss-preset-env-error';
+}
